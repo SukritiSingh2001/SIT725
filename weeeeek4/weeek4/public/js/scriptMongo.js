ad2e4108243638cce355a2d0c9eeca6d05dedc9c
SukritiSingh2001 <s.sukriti2001@gmail.com>
2024-08-12 21:17:01 +1000

    Hello everyone

---
+++ weeeeek4/weeek4/public/js/scriptMongo.js
@@ -0,0 +1,55 @@
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
+                '</div><div class="card-content">'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>'+
+                '<div class="card-reveal">'+
+                '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+                '<p class="card-text">'+item.description+'</p>'+
+                '</div></div></div>';
+        $("#card-section").append(itemToAppend)
+    });
+}
+
+const formSubmitted = () => {
+    let formData = {};
+    formData.name = $('#name').val();
+    formData.email = $('#email').val();
+    formData.message = $('#message').val();
+    
+
+    console.log("form data submitted",formData);
+    postContact(formData);
+}
+
+function postContact(contact){
+    $.ajax({
+        url:'/api/contact',
+        type:'POST',
+        data:contact,
+        success: (result)=>{
+            if (result.statusCode === 201) {
+                alert('contact post successful');
+            }
+        }
+    });
+}
+
+function getAllData(){
+    $.get('/api/cats', (response)=>{
+        // response's data is in array format, so we can use it
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+    });
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        formSubmitted();
+    });
+    $('.modal').modal();
+    getAllData();
+});
